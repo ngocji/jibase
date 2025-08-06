076d3985ff3c9cc144c58a43e9f0a135673257ea
ngocji <dngocx@gmail.com>
2025-08-06 20:55:13 +0700

    Add flow cache data

--- jibase/src/main/java/com/jibase/flow/cache/DataCacheManager.kt
+++ jibase/src/main/java/com/jibase/flow/cache/DataCacheManager.kt
@@ -3,14 +3,43 @@ package com.jibase.flow.cache
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @Suppress("UNCHECKED_CAST")
 class DataCacheManager(private val scope: CoroutineScope) {
     private val dataCache = mutableMapOf<String, CacheModel<*, *>>()
+    private val flowJobs = mutableMapOf<String, Job>()
+
+    fun <T> getFlow(
+        cacheKey: String,
+        initialValue: T,
+        context: CoroutineDispatcher = Dispatchers.IO,
+        forceRefresh: Boolean = false,
+        flowProvider: () -> Flow<T>
+    ): Flow<T> {
+        val existing = dataCache[cacheKey] as? CacheModel<T, *>
+        if (!forceRefresh && existing != null) {
+            return existing.stateFlow
+        }
+
+        val newStateFlow = existing?.stateFlow ?: MutableStateFlow(initialValue)
+        val newCacheModel = CacheModel(null, { flowProvider() }, newStateFlow)
+        dataCache[cacheKey] = newCacheModel
+
+        // cancel prev
+        flowJobs.remove(cacheKey)?.cancel()
+        val job = scope.launch(context) {
+            flowProvider()
+                .collectLatest { newStateFlow.tryEmit(it) }
+        }
+        flowJobs[cacheKey] = job
+        return newStateFlow
+    }
 
     fun <T, P> getData(
         cacheKey: String,
@@ -40,9 +69,11 @@ class DataCacheManager(private val scope: CoroutineScope) {
 
     fun clearCache(cacheKey: String) {
         dataCache.remove(cacheKey)
+        flowJobs.remove(cacheKey)?.cancel()
     }
 
     fun clearAllCache() {
         dataCache.clear()
+        flowJobs.onEach { (_, job) -> job.cancel() }.clear()
     }
 }
