73dbdcd3df4a4daf602f33de95a4cabc9527c82d
ngocji <dngocx@gmail.com>
2025-11-15 20:54:53 +0700

    Fix binding

--- jibase/src/main/java/com/jibase/extensions/ViewBindingDelegate.kt
+++ jibase/src/main/java/com/jibase/extensions/ViewBindingDelegate.kt
@@ -8,28 +8,35 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 class FragmentViewBindingDelegate<T : ViewBinding>(
-        val fragment: Fragment,
-        val viewBindingFactory: (View) -> T
+    val fragment: Fragment,
+    val viewBindingFactory: (View) -> T
 ) : ReadOnlyProperty<Fragment, T> {
     private var binding: T? = null
 
     init {
-        fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
-            override fun onCreate(owner: LifecycleOwner) {
-                fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
-                    viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
-                        override fun onDestroy(owner: LifecycleOwner) {
-                            binding = null
-                        }
-                    })
+        fragment.lifecycle.addObserver(object : LifecycleEventObserver {
+            override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+                if (event == Lifecycle.Event.ON_CREATE) {
+                    fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
+                        viewLifecycleOwner.lifecycle.addObserver(object : LifecycleEventObserver {
+                            override fun onStateChanged(
+                                source: LifecycleOwner,
+                                event: Lifecycle.Event
+                            ) {
+                                if (event == Lifecycle.Event.ON_DESTROY) {
+                                    binding = null
+                                }
+                            }
+                        })
+                    }
                 }
             }
         })
@@ -51,19 +58,19 @@ class FragmentViewBindingDelegate<T : ViewBinding>(
 }
 
 fun <T : ViewBinding> Fragment.viewBinding(viewBindingFactory: (View) -> T) =
-        FragmentViewBindingDelegate(this, viewBindingFactory)
+    FragmentViewBindingDelegate(this, viewBindingFactory)
 
 inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
-        crossinline bindingInflater: (LayoutInflater) -> T
+    crossinline bindingInflater: (LayoutInflater) -> T
 ) = lazy(LazyThreadSafetyMode.NONE) {
     bindingInflater.invoke(layoutInflater)
 }
 
 inline fun <T : ViewBinding> ViewGroup.viewBinding(
-        viewBindingFactory: (LayoutInflater, ViewGroup, Boolean) -> T,
-        isConnectedToParent: Boolean = false
+    viewBindingFactory: (LayoutInflater, ViewGroup, Boolean) -> T,
+    isConnectedToParent: Boolean = false
 ) = viewBindingFactory.invoke(LayoutInflater.from(context), this, isConnectedToParent)
 
 inline fun <T : ViewBinding> Context.viewBinding(
-        viewBindingFactory: (LayoutInflater) -> T,
+    viewBindingFactory: (LayoutInflater) -> T,
 ) = viewBindingFactory.invoke(LayoutInflater.from(this))
